15db5297386b4777eaa4f8ea057e5ade3e724430
Miku <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2023-12-19 15:56:45 +0100

    version 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,48 @@
 import './App.css';
 import Link from './Links';
+import React, { useState } from 'react';
+
+import profilePic from './aliki.jpg';
+
+import imageLinkedIn from './logos/linkedIn.png';
 
 function App() {
+
+  const [theme, setTheme] = useState('default'); // Initialize with a default theme
+
+  const changeTheme = () => {
+    // Define an array of available themes
+    const themes = ['default', 'theme1', 'theme2', 'theme3',];
+
+    // Find the index of the current theme in the array
+    const currentIndex = themes.indexOf(theme);
+
+    // Calculate the index of the next theme
+    const nextIndex = (currentIndex + 1) % themes.length;
+
+    // Set the next theme
+    setTheme(themes[nextIndex]);
+  };
+
   return (
-    <div className="App">
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
+    <div className={`app ${theme}`}>
+      <br />
+      <br />
+      <div className="profile-container">
+        <img
+          src={profilePic}
+          alt="Profile"
+          className={`profile-picture ${theme}Child`}
+        />
+      </div>
       <h1>Aliki Katsimpa</h1>
-      <Link name={"LinkedIn"} link={"https://www.linkedin.com/in/alikikatsimpa/"}/>
-      <Link name={"test2"} link={"test2"}/>
+      <Link name={"LinkedIn"} link={"https://www.linkedin.com/in/alikikatsimpa/"} logo={imageLinkedIn} theme={theme} />
+      <Link name={"blank1"} link={"https://www.linkedin.com/in/alikikatsimpa/"} logo={imageLinkedIn} theme={theme} />
+      <Link name={"blank2"} link={"https://www.linkedin.com/in/alikikatsimpa/"} logo={imageLinkedIn} theme={theme} />
+      <Link name={"blank3"} link={"https://www.linkedin.com/in/alikikatsimpa/"} logo={imageLinkedIn} theme={theme} />
+      
+      <button onClick={changeTheme} className={`button ${theme}Child`}>Change Theme</button>
+
     </div>
   );
 }
--- src/Links.js
+++ src/Links.js
@@ -3,10 +3,11 @@ import React from 'react';
 import './Link.css';
 
 function Link(props) {
-    const { link, name } = props;
+    const { link, name, logo, theme} = props;
     return (
-        <a href={link} className="Link">
-            <p className="Text">{name}</p>
+        <a href={link} className={`Link ${theme}Child`}>
+            <p className="Text">{"‎ ‎" + name}</p>
+            <img src={logo} alt={""} className="Logo" />
         </a>
     );
 }
